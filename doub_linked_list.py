8e07cd548dd6181e1b0afa9e7edda20ba9a7eae3
kayham2 <kevinmabraham@gmail.com>
2020-04-24 19:07:47 -0400

    Redid dll

--- doub_linked_list.py
+++ doub_linked_list.py
@@ -1,19 +1,23 @@
-class Node:
+class Node():
 	def __init__(self, data):
 		self.data = data
 		self.next = None
 		self.prev = None
 
-class DoublyLinkedList:
+class Doubly_Linked_list():
 	def __init__(self):
 		self.head = None
+
 	def append(self, data):
 		new_node = Node(data)
-		if self.head == None:
-			self.head = new_node
+		current = self.head
+
+		if self.head is None:
 			new_node.prev = None
+			new_node.next = None
+			self.head = new_node
+
 		else:
-			current = self.head
 			while current.next:
 				current = current.next
 			current.next = new_node
@@ -21,145 +25,118 @@ class DoublyLinkedList:
 			new_node.next = None
 		return
 
-
 	def prepend(self, data):
 		new_node = Node(data)
-		if self.head == None:
-			self.head = new_node
+
+		if self.head is None:
 			new_node.prev = None
 			new_node.next = None
+			self.head = new_node
 		else:
 			current = self.head
-			self.head = new_node
 			new_node.next = current
 			current.prev = new_node
+			new_node.prev = None
+			self.head = new_node
 		return
 
-	def add_after(self, data, key):
-		new_node = Node(data)
+	def display_list(self):
 		current = self.head
 		while current:
-			if current.next is None and current.data == key:
-				self.append(new_node.data)
-				return
-			elif current.data == key:
-				tmp = current.next
-				current.next = new_node
-				new_node.prev = current
-				new_node.next = tmp
-				tmp.prev = new_node
-				return
+			print(current.data)
 			current = current.next
 		return
 
-
-	def add_before(self, data, key):
-		new_node = Node(data)
-		current = self.head
-		while current:
-			if current.prev is None and current.data == key:
-				self.prepend(new_node.data)
-				return
-			elif current.data == key:
-				tmp = current.prev
-				tmp.next = new_node
-				new_node.prev = tmp
-				new_node.next = current
-				current.prev = new_node
-				return
-			current	= current.next
-		return
-
-	def delete_node(self, key):
+	def delete (self, data):
 		current = self.head
 		prev = None
-		while current:
-			if current.data == key and current == self.head:
-				if current.next is None:
-					self.head = None
-					current = None
-					return
-				nxt = current.next
-				self.head = nxt
-				current.next = None
-				current = None
+		if self.head is None:
+			return
+		elif self.head.next is None:
+			self.head = None
+			return
+		elif self.head.data == data:
+			self.head = self.head.next
+			self.head.prev = None
+		else:
+			while current.next and current.data != data:
+				prev = current
+				current = current.next
+			if current.data != data:
 				return
+			elif current.next is None:
+				prev.next = None
+				current.prev = None
 			else:
-				if current.data == key:
-					if current.next is None:
-						prev = current.prev
-						prev.next = None
-						current = None
-						return
-					prev = current.prev
-					nxt = current.next
-					prev.next = current.next
-					nxt.prev = prev
-					return
-			current = current.next
-		return
+				prev.next = current.next
+				current.prev = None
+				current.next = None
+			return	
 
 	def reverse(self):
 		current = self.head
-		tmp = None
+		prev = None
 		while current:
-			tmp = current.prev
+			prev = current.prev
 			current.prev = current.next
-			current.next = tmp
+			current.next = prev
 			current = current.prev
-		if tmp:
-			self.head = tmp.prev
+		if prev:
+			self.head = prev.prev
 		return
 
 	def remove_duplicates(self):
 		current = self.head
-		tmp = None
-		dups = []
+		prev = None
+		li = []
+
 		while current:
-			if current.data in dups:
-				tmp = current.prev
-				tmp.next = current.next
-				if current.next:
-					current.next.prev = tmp
+			if current.data in li:
+				tmp = current.next
+				prev.next = current.next
+				current.next = None
+				current.prev = None
+				current = tmp
 			else:
-				dups.append(current.data)
-			current = current.next
+				li.append(current.data)
+				prev = current
+				current = current.next
 		return
 
-	def pairs_with_sums(self, sum_val):
+	def pairs_with_sums(self, number):
 		current = self.head
-		pairs = []
-		while current:
-			tmp = current.next
-			while tmp:
-				if(tmp.data + current.data == sum_val):
-					pairs.append(f"{current.data} and {tmp.data}")
-					break
-				else:
-					tmp = tmp.next
+		current2 = None
+		pairs = list()
+
+		while current.next:
+			current2 = current.next
+			while current2.next:
+				if current.data + current2.data == number:
+					pairs.append(f"({current.data}, {current2.data})")
+				current2 = current2.next
 			current = current.next
 		return pairs
 
-	def print_list(self):
-		current = self.head
-		while current:
-			print(current.data, "Hi")
-			current = current.next
-		return
 
-
-dllist = DoublyLinkedList()
+dllist = Doubly_Linked_list()
 dllist.append(1)
 dllist.append(2)
 dllist.append(3)
 dllist.append(4)
 dllist.append(5)
+# dllist.append(8)
+# dllist.append(4)
+# dllist.append(10)
+# dllist.append(12)
+# dllist.append(12)
+dllist.display_list()
 
+print("\n")
 
+# dllist.delete(5)
+# dllist.reverse()
+# dllist.remove_duplicates()
 
-# dllist.add_before(0, 1)
-# dllist.delete_node(2)
-#dllist.reverse()
-print(dllist.pairs_with_sums(0))
+print(dllist.pairs_with_sums(5))
+# dllist.display_list()
 
-#dllist.print_list()
